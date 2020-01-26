9039be31429818f91db550fe1ff4c838b88c0b27
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2020-01-26 10:04:23 +0100

    add tf2 support to deint module

--- tvb_algo/deint.py
+++ tvb_algo/deint.py
@@ -2,6 +2,7 @@
 """
 
 import numpy as np
+import tensorflow as tf
 
 
 def em_white(f, g, dt, x):
@@ -9,10 +10,14 @@ def em_white(f, g, dt, x):
     """
     i = 0
     nd = x.shape
+    if tf.is_tensor(x):
+        G = lambda i, x: tf.random.normal(nd) * tf.sqrt(g(i, x))
+    else:
+        G = lambda i, x: np.random.randn(*nd) * np.sqrt(g(i, x))
     while True:
         yield x
         i += 1
-        x += dt * f(i, x) + np.random.randn(*nd) * np.sqrt(g(i, x))
+        x += dt * f(i, x) + G(i, x)
 
 
 def em_color(f, g, dt, lam, x):
@@ -20,11 +25,19 @@ def em_color(f, g, dt, lam, x):
     """
     i = 0
     nd = x.shape
-    e = np.sqrt(g(i, x) * lam) * np.random.randn(*nd)
-    E = np.exp(-lam * dt)
+    if tf.is_tensor(x):
+        N = lambda : tf.random.normal(nd)
+        sqrt = tf.sqrt
+        e = tf.sqrt(g(i, x) * lam) * N()
+        E = tf.exp(-lam * dt)
+    else:
+        N = lambda : np.random.randn(*nd)
+        sqrt = np.sqrt
+        e = np.sqrt(g(i, x) * lam) * N()
+        E = np.exp(-lam * dt)
     while True:
         yield x, e
         i += 1
         x += dt * (f(i, x) + e)
-        h = np.sqrt(g(i, x) * lam * (1 - E ** 2)) * np.random.randn(*nd)
+        h = sqrt(g(i, x) * lam * (1 - E ** 2)) * N()
         e = e * E + h
